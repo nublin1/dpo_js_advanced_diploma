a21cb591bda0910c63ff742f519a8b94e450e7a6
alex <reddragon1151@hotmail.com>
2023-07-23 14:35:45 +0300

    fix

--- front/src/js/pages/loginPage.js
+++ front/src/js/pages/loginPage.js
@@ -1,9 +1,7 @@
 import { el } from "redom";
-import {
-  login,  
-  getUserAccounts,  
-} from "../api";
+import { login, getUserAccounts } from "../api";
 import { renderAccountsPage } from "./accountsPage.js";
+import JustValidate from "just-validate";
 
 export function renderSignInPage() {
   const container = el("div", {
@@ -11,7 +9,7 @@ export function renderSignInPage() {
   });
 
   const h1 = el("h1", "Вход в аккаунт");
-  const form = el("form");
+  const form = el("form", { id: "signInForm" });
   const fieldset = el("fieldset");
   fieldset.style.width = "100%";
 
@@ -19,44 +17,69 @@ export function renderSignInPage() {
     class: " ",
   });
   const labelLogin = el("label", { class: "" }, "Логин");
-  const loginInput = el("input", { class: "", value: "developer" });
+  const loginInput = el("input", { class: "input-login", value: "developer" });
   loginWrapper.append(labelLogin, loginInput);
 
   const passwordWrapper = el("div", {
     class: " ",
   });
   const labelPassword = el("label", { class: "" }, "Пароль");
-  const passwordInput = el("input", { class: "", value: "skillbox" });
+  const passwordInput = el("input", {
+    class: "input-password",
+    value: "skillbox",
+  });
   passwordWrapper.append(labelPassword, passwordInput);
 
-  const buttonWrapper = el("div", { class: "buttons", });
+  const buttonWrapper = el("div", { class: "buttons" });
   const button = el(
     "button",
     { class: "button button-primary", id: "signIn" },
     "Войти"
   );
-  button.addEventListener("click", async (e) => {
-    e.preventDefault();
-    let response = await login(loginInput.value, passwordInput.value);
-    if (response.error === "") {
-      let response2 = await getUserAccounts();
-      //console.log(response2);
-      if (response2) {
-        window.location.hash = '#' + "accounts";
-      }
-    }
-  });
-
   buttonWrapper.append(button);
   fieldset.append(loginWrapper, passwordWrapper);
   form.append(fieldset, buttonWrapper);
   container.append(h1, form);
 
   //
-  
+
   document.getElementById("header-buttons").style.display = "none";
   const main = document.getElementById("main");
   main.innerHTML = "";
   main.appendChild(container);
+
+  configLogin();
 }
 
+function configLogin() {
+  const loginValidator = new JustValidate("#signInForm", {});
+
+  loginValidator.addField(document.querySelector(".input-login"), [
+    {
+      rule: "required",
+      errorMessage: "Введите логин",
+    },
+  ])
+  .addField(document.querySelector(".input-password"), [
+    {
+      rule: "required",
+      errorMessage: "Введите пароль",
+    }
+  ])
+
+  loginValidator.onSuccess(function (e) {
+    e.preventDefault();   
+    tryLogin();
+  })
+}
+
+async function tryLogin() {
+  let response = await login(document.querySelector(".input-login").value, document.querySelector(".input-password").value);
+  if (response.error === "") {
+    let response2 = await getUserAccounts();
+    //console.log(response2);
+    if (response2) {
+      window.location.hash = "#" + "accounts";
+    }
+  }
+}
